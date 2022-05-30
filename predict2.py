bdf02fd4548d6deabfa530b0fe42263c29d80d00
AhmadJamal01 <65499354+AhmadJamal01@users.noreply.github.com>
2022-05-30 15:19:11 +0200

    Decision Tree classifier
    Added decision tree classification. Attempted to use hinge + COLD features for final evaluation, won't be used. Calculated average times for each classifier

--- predict2.py
+++ predict2.py
@@ -7,6 +7,7 @@ import cv2
 import numpy as np
 import imutils
 import hinge_and_cold_feature_extraction.src.hinge_feature_extraction as hcf
+import hinge_and_cold_feature_extraction.src.cold_feature_extraction as cf
 
 parser = argparse.ArgumentParser(
     description="take full paths for input and output files"
@@ -24,8 +25,8 @@ if not os.path.exists(test_path):
     exit()
 
 
-filename = "hinge_rfc_checkpoint.ckpt"
-if os.path.exists("hinge_rfc_checkpoint.ckpt"):
+filename = "hinge_cold_checkpoint.ckpt"
+if os.path.exists("hinge_cold_checkpoint.ckpt"):
     model = pickle.load(open(filename, "rb"))
 
     if os.path.exists(os.path.join(out_path, "results.txt")):
@@ -44,12 +45,22 @@ if os.path.exists("hinge_rfc_checkpoint.ckpt"):
     for img_path in test_img_paths:
         test_img = cv2.imread(img_path, 0)
         hinge_classifier = hcf.Hinge((10, 3, False, True))
+        cold_classifier = cf.Cold((10, 3, False, True))
         start = time.time()
-        img_feautres = hinge_classifier.get_hinge_features(test_img)
-        img_feautres = np.reshape(img_feautres, (1, -1))
         try:
-            prediction = 1 - model.predict(img_feautres)[0]
+            img_hinge_features = hinge_classifier.get_hinge_features(test_img)
+            img_cold_features = cold_classifier.get_cold_features(test_img)
+            img_features = []
+            # for i in range(len(img_hinge_features)):
+            #     img_features.append(
+            #         np.concatenate((img_hinge_features[i], img_cold_features[i]))
+            #     )
+            img_features = np.concatenate([img_hinge_features, img_cold_features])
+            img_features = np.array(img_features, dtype="float64")
+            img_features = np.reshape(img_features, (1, -1))
+            prediction = 1 - model.predict(img_features)[0]
         except Exception as e:
+            print(e)
             prediction = -1
         duration = time.time() - start
         if duration == 0:
